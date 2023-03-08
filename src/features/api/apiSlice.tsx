52da23636faeb25c9728100dfeb666c4feb169e6
warlockja <warlockja@gmail.com>
2023-03-08 19:53:26 +0400

    apiSlice idgaf

--- src/features/api/apiSlice.tsx
+++ src/features/api/apiSlice.tsx
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { ITodo } from '../todos/TodosList'
 
 const BASE_URL = 'http://localhost:3500'
 
@@ -6,7 +7,7 @@ export const apiSlice = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
     endpoints: builder => ({
-        getTodos: builder.query({
+        getTodos: builder.query<ITodo[], void>({
             query: () => '/todos'
         })
     })
--- src/features/todos/TodosList.tsx
+++ src/features/todos/TodosList.tsx
@@ -1,25 +1,52 @@
-import { useAppSelector } from "../../app/hooks"
+import Spinner from "../../util/Spinner"
+import { useGetTodosQuery } from "../api/apiSlice"
 import AddTodo from "./AddTodo"
 
-const TodosList = () => {
-    const todos = useAppSelector((state) => state.todos)
+export interface ITodo {
+    userId: number;
+    id: number;
+    title: string;
+    completed: boolean;
+}
 
-    const todosList = todos.map(todo => 
-        <li key={todo.id}>
-            <p>{todo.title}</p>
-            <div className="todoFooter">
-                <p>{todo.userId}</p>
-                <p>{todo.completed ? 'Completed' : 'Not completed'}</p>
-            </div>
-        </li>
-    )
+const TodosList = () => {
+    const {
+        data: todos,
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    } = useGetTodosQuery()
+
+    let content
+    if(isLoading) {
+        content = <>
+            <Spinner />
+            <Spinner />
+            <Spinner />
+            <Spinner />
+            <Spinner />
+        </>
+    } else if (isError) {
+        content = <p>{error.toString()}</p>
+    } else if (isSuccess) {
+        content = todos.map(todo => 
+            <li key={todo.id}>
+                <p>{todo.title}</p>
+                <div className="todoFooter">
+                    <p>{todo.userId}</p>
+                    <p>{todo.completed ? 'Completed' : 'Not completed'}</p>
+                </div>
+            </li>
+        )
+    }
 
     return (
         <section className="todos">
             <h2>Todos</h2>
             <AddTodo />
             <ul>
-                {todosList}
+                {content}
             </ul>
         </section>
     )
