9edb10587f1e81f010d3bc27408d61288d1a4201
warlockja <warlockja@gmail.com>
2023-04-24 16:30:45 +0400

    vercel testing

--- src/util/useGetGeodata.tsx
+++ src/util/useGetGeodata.tsx
@@ -2,7 +2,6 @@ import axios from "axios";
 
 export const getGeodata = async()=>{
     const res = await axios.get('https://geolocation-db.com/json/')
-    console.log(res.data);
     return res
 }
 
