9fdc1924bf02bba97d975892be7a655f61f0d4fd
warlockja <warlockja@gmail.com>
2023-05-12 18:23:20 +0400

    currency conversion

--- src/features/FreeCurrencyApi/FreeCurrencyApi.tsx
+++ src/features/FreeCurrencyApi/FreeCurrencyApi.tsx
@@ -39,19 +39,36 @@ const API_CURRENCY = {
 const FreeCurrencyApi = () => {
     const [currencyList, setCurrencyList] = useState(API_CURRENCY)
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        console.log(Object.entries(currencyList))
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>, currencyCode: string) => {
+        // const newCurrencyList = Object.entries(currencyList).forEach((key) => (key))
 
-        return event.target.value
+        let newCurrencyList = {}
+        const newValue = Number(event.target.value)
+        if(!isNaN(newValue)) {
+            Object.entries(API_CURRENCY).map((key) => key[0] === currencyCode ? newCurrencyList[key[0]] = newValue : newCurrencyList[key[0]] = (key[1] / API_CURRENCY[currencyCode] * newValue).toFixed(4))
+        }
+
+        // console.log(newCurrencyList)
+        return newCurrencyList
     }
 
     return (
         <section className="freeCurrencyApi">
+            <h2>Currency Exchange</h2>
             <label htmlFor="freeCurrencyApi__mainCurrency">USD</label>
-            <input id="freeCurrencyApi__mainCurrency" type="text" defaultValue={currencyList.USD} onChange={(e) => handleChange(e)}/>
+            <input
+                id="freeCurrencyApi__mainCurrency"
+                type="number"
+                value={currencyList.USD}
+                onChange={(e) => setCurrencyList(handleChange(e, 'USD'))}
+            />
             <div className="freeCurrentApi__exchangeCurrencyWrapper">
                 <label htmlFor="exchangeCurrencyWrapper__mainCurrency">TRY</label>
-                <input id="exchangeCurrencyWrapper__mainCurrency" type="text" defaultValue={currencyList.TRY} onChange={(e) => handleChange(e)}/>
+                <input id="exchangeCurrencyWrapper__mainCurrency" type="text" value={currencyList.TRY} onChange={(e) => setCurrencyList(handleChange(e, 'TRY'))}/>
+            </div>
+            <div className="freeCurrentApi__exchangeCurrencyWrapper">
+                <label htmlFor="exchangeCurrencyWrapper__mainCurrency">RUB</label>
+                <input id="exchangeCurrencyWrapper__mainCurrency" type="text" value={currencyList.RUB} onChange={(e) => setCurrencyList(handleChange(e, 'RUB'))}/>
             </div>
         </section>
     )
