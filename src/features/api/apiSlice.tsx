b37d484b251186455958bd5d9f050c23083306d5
warlockja <warlockja@gmail.com>
2023-08-30 19:06:52 +0300

    fixing refresh api route

--- src/features/api/apiSlice.tsx
+++ src/features/api/apiSlice.tsx
@@ -1,8 +1,8 @@
 import {
-    BaseQueryFn,
-    createApi,
-    fetchBaseQuery,
-    MutationDefinition,
+  BaseQueryFn,
+  createApi,
+  fetchBaseQuery,
+  MutationDefinition,
 } from "@reduxjs/toolkit/query/react";
 import { setCredentials, logOut, AccessTokenType } from "./auth/authSlice";
 import { RootState } from "../../app/store";
@@ -11,136 +11,134 @@ import { MutationTrigger } from "@reduxjs/toolkit/dist/query/react/buildHooks";
 const BASE_URL = import.meta.env.VITE_APP_BASE_URI;
 
 export interface IRTKQuery {
-    login?: MutationTrigger<
-        MutationDefinition<
-            any,
-            BaseQueryFn<any, unknown, unknown, {}, {}>,
-            "Todos" | "Auth",
-            any,
-            "api"
-        >
-    >;
-    gLogin?: MutationTrigger<
-        MutationDefinition<
-            any,
-            BaseQueryFn<any, unknown, unknown, {}, {}>,
-            "Todos" | "Auth",
-            any,
-            "api"
-        >
-    >;
-    isLoading: boolean;
-    isError?: boolean;
-    error?: unknown;
+  login?: MutationTrigger<
+    MutationDefinition<
+      any,
+      BaseQueryFn<any, unknown, unknown, {}, {}>,
+      "Todos" | "Auth",
+      any,
+      "api"
+    >
+  >;
+  gLogin?: MutationTrigger<
+    MutationDefinition<
+      any,
+      BaseQueryFn<any, unknown, unknown, {}, {}>,
+      "Todos" | "Auth",
+      any,
+      "api"
+    >
+  >;
+  isLoading: boolean;
+  isError?: boolean;
+  error?: unknown;
 }
 
 interface IApiErrorResponse {
-    error: {
-        originalStatus: number;
-    };
+  error: {
+    originalStatus: number;
+  };
 }
 
 interface IAuthApiError {
-    data: {
-        message: string;
-    };
+  data: {
+    message: string;
+  };
 }
 
 interface IAuthRegisterError {
-    originalStatus: number;
-    data: string;
+  originalStatus: number;
+  data: string;
 }
 
 interface IRefreshTokenResult {
-    data?: {
-        accessToken: AccessTokenType;
-    };
+  data?: {
+    accessToken: AccessTokenType;
+  };
 }
 
 // checks response error type to ensure it came from the API
 export function isApiRefreshError(error: unknown): error is IApiErrorResponse {
-    return (
-        typeof error === "object" &&
-        error != null &&
-        "error" in error &&
-        typeof (error as any).error === "object" &&
-        "originalStatus" in (error as any).error &&
-        typeof (error as any).error.originalStatus === "number"
-    );
+  return (
+    typeof error === "object" &&
+    error != null &&
+    "error" in error &&
+    typeof (error as any).error === "object" &&
+    "originalStatus" in (error as any).error &&
+    typeof (error as any).error.originalStatus === "number"
+  );
 }
 
 // confirming that login error came from the API by verifying its type
 export function isApiAuthError(error: unknown): error is IAuthApiError {
-    return (
-        typeof error === "object" &&
-        error != null &&
-        "data" in error &&
-        typeof (error as any).data === "object" &&
-        "message" in (error as any).data &&
-        typeof (error as any).data.message === "string"
-    );
+  return (
+    typeof error === "object" &&
+    error != null &&
+    "data" in error &&
+    typeof (error as any).data === "object" &&
+    "message" in (error as any).data &&
+    typeof (error as any).data.message === "string"
+  );
 }
 
 // confirming that login error came from the API by verifying its type
 export function isApiRegisterError(
-    error: unknown
+  error: unknown
 ): error is IAuthRegisterError {
-    return (
-        typeof error === "object" &&
-        error != null &&
-        "data" in error &&
-        typeof (error as any).data === "string" &&
-        "originalStatus" in error &&
-        typeof (error as any).originalStatus === "number"
-    );
+  return (
+    typeof error === "object" &&
+    error != null &&
+    "data" in error &&
+    typeof (error as any).data === "string" &&
+    "originalStatus" in error &&
+    typeof (error as any).originalStatus === "number"
+  );
 }
 
 // attaching access token to every request
 const baseQuery = fetchBaseQuery({
-    baseUrl: BASE_URL,
-    credentials: "include",
-    prepareHeaders: (headers, { getState }) => {
-        const token = (getState() as RootState).auth.accessToken;
-        if (token) {
-            headers.set("authorization", `Bearer ${token}`);
-        }
-        return headers;
-    },
+  baseUrl: BASE_URL,
+  credentials: "include",
+  prepareHeaders: (headers, { getState }) => {
+    const token = (getState() as RootState).auth.accessToken;
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`);
+    }
+    return headers;
+  },
 });
 
 // wrapping baseQuery into reauth for when access token expires
 const baseQueryWithReauth: BaseQueryFn = async (args, api, extraOptions) => {
-    let result = await baseQuery(args, api, extraOptions);
+  let result = await baseQuery(args, api, extraOptions);
 
-    // checking error type ensuring it came from the API
-    if (isApiRefreshError(result)) {
-        if (result.error.originalStatus === 403) {
-            // sending refresh token to get new access token
-            const refreshResult = (await baseQuery(
-                "/refresh",
-                api,
-                extraOptions
-            )) as IRefreshTokenResult;
-            if (refreshResult?.data) {
-                const idToken = (api.getState() as RootState).auth.idToken;
-                console.log("Refresh request");
-                // store new token
-                api.dispatch(
-                    setCredentials({ ...refreshResult.data, idToken })
-                );
-                // retry the oirignal query with new access token
-                result = await baseQuery(args, api, extraOptions);
-            } else {
-                api.dispatch(logOut());
-            }
-        }
+  // checking error type ensuring it came from the API
+  if (isApiRefreshError(result)) {
+    if (result.error.originalStatus === 403) {
+      // sending refresh token to get new access token
+      const refreshResult = (await baseQuery(
+        "/v1/refresh",
+        api,
+        extraOptions
+      )) as IRefreshTokenResult;
+      if (refreshResult?.data) {
+        const idToken = (api.getState() as RootState).auth.idToken;
+        console.log("Refresh request");
+        // store new token
+        api.dispatch(setCredentials({ ...refreshResult.data, idToken }));
+        // retry the oirignal query with new access token
+        result = await baseQuery(args, api, extraOptions);
+      } else {
+        api.dispatch(logOut());
+      }
     }
+  }
 
-    return result;
+  return result;
 };
 
 export const apiSlice = createApi({
-    baseQuery: baseQueryWithReauth,
-    // tagTypes: ['Todos', 'User'],
-    endpoints: (builder) => ({}),
+  baseQuery: baseQueryWithReauth,
+  // tagTypes: ['Todos', 'User'],
+  endpoints: (builder) => ({}),
 });
